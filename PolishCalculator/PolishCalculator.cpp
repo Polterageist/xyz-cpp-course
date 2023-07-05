14a0ed9e0e1e11c80fd0ecfa637c0118bff94f5d
Aleksandr Rybalka <polterageist@gmail.com>
2023-07-05 12:45:41 +0400

    Added cycle into polish calculator (l1-2-4)

--- PolishCalculator/PolishCalculator.cpp
+++ PolishCalculator/PolishCalculator.cpp
@@ -2,32 +2,41 @@
 
 int main()
 {
-	std::cout << "Enter operation (+ - / *) and operands in format op a b!\n";
-	std::string operation;
-	double a = 0.;
-	double b = 0.;
-	std::cin >> operation >> a >> b;
-	double result = 0.;
-	if (operation == "+")
+	bool shouldContinue = true;
+	while (shouldContinue)
 	{
-		result = a + b;
-	}
-	else if (operation == "-")
-	{
-		result = a - b;
-	}
-	else if (operation == "*")
-	{
-		result = a * b;
-	}
-	else if (operation == "/")
-	{
-		result = a / b;
-	}
-	else
-	{
-		std::cout << "Unknown operation: " << operation << "\n";
-	}
+		std::cout << "Enter operation (+ - / *) and operands in format op a b!\n";
+		std::string operation;
+		double a = 0.;
+		double b = 0.;
+		std::cin >> operation >> a >> b;
+		double result = 0.;
+		if (operation == "+")
+		{
+			result = a + b;
+		}
+		else if (operation == "-")
+		{
+			result = a - b;
+		}
+		else if (operation == "*")
+		{
+			result = a * b;
+		}
+		else if (operation == "/")
+		{
+			result = a / b;
+		}
+		else
+		{
+			std::cout << "Unknown operation: " << operation << "\n";
+		}
 
-	std::cout << "Result: " << result << "\n";
+		std::cout << "Result: " << result << "\n";
+
+		std::cout << "Do you want to continue? (y/n)\n";
+		std::string answer;
+		std::cin >> answer;
+		shouldContinue = answer == "y";
+	}
 }
