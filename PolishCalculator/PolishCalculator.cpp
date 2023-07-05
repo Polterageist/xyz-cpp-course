0f3c26857e679381008544a78d151dd72c9c5022
Aleksandr Rybalka <polterageist@gmail.com>
2023-07-05 12:38:56 +0400

    Added polish notation calculator (l1-2-3)

---
+++ PolishCalculator/PolishCalculator.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Enter operation (+ - / *) and operands in format op a b!\n";
+	std::string operation;
+	double a = 0.;
+	double b = 0.;
+	std::cin >> operation >> a >> b;
+	double result = 0.;
+	if (operation == "+")
+	{
+		result = a + b;
+	}
+	else if (operation == "-")
+	{
+		result = a - b;
+	}
+	else if (operation == "*")
+	{
+		result = a * b;
+	}
+	else if (operation == "/")
+	{
+		result = a / b;
+	}
+	else
+	{
+		std::cout << "Unknown operation: " << operation << "\n";
+	}
+
+	std::cout << "Result: " << result << "\n";
+}
