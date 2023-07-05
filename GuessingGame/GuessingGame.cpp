c6d6b5ad0964a1fab441935845b4420eca2569ad
Aleksandr Rybalka <polterageist@gmail.com>
2023-07-05 15:06:35 +0400

    Added computer's turn into guessing game (l1-3-1)

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -9,17 +9,17 @@ int main()
     // Initialize random seed
     srand((unsigned)time(nullptr));
 
-    while (true)
-    {
+	while (true)
+	{
 		int secretNumber = rand() % 101 + 1;
 		std::cout << "'I've thought of a number between 1 and 100. How fast can you guess it?\n";
 
 		bool isGuessed = false;
-		int amountOfTries = 0;
+		int playerAmountOfTries = 0;
 		while (!isGuessed)
 		{
 			std::cout << "Your try: ";
-			++amountOfTries;
+			++playerAmountOfTries;
 			int guess = 0;
 			std::cin >> guess;
 			if (guess < secretNumber)
@@ -37,22 +37,101 @@ int main()
 			}
 		}
 
-		std::cout << "You guessed it in " << amountOfTries << " tries!\n";
-		if (amountOfTries > 10)
+		std::cout << "You guessed it in " << playerAmountOfTries << " tries!\n";
+		if (playerAmountOfTries > 10)
 		{
 			std::cout << "You are not very good at this, are you?\n";
 		}
-		else if (amountOfTries > 8)
+		else if (playerAmountOfTries > 8)
 		{
 			std::cout << "Not bad, but you can do better!\n";
 		}
-		else if (amountOfTries > 1)
+		else if (playerAmountOfTries > 1)
 		{
 			std::cout << "You know the trick!\n";
 		}
 		else
 		{
 			std::cout << "You're lucky thing!\n";
+		}
+
+		// Pseudo code for computer guessing:
+		// Init computers turn
+		// Init min and max as 1 and 100
+		// While not guessed or computer detected cheating
+		//   increase tries counter
+		//   take middle between min and max
+		//   ask player if it is guessed
+		//   if player said yes
+		//       exit cycle
+		//   else
+		//     ask player if it is too low
+		//     if player said yes
+		//	     set min to guess + 1
+		//     else
+		//       set max to guess - 1
+		//	
+		//     if min >= max
+		//		     say player "You are cheating!" and exit cycle
+		// 
+		// if computer tries less than player tries
+		//   say "I won!"
+		// else if computer tries more than player tries
+		//   say "You won!"
+		// else
+		//   say "It's a draw!"
+
+
+		std::cout << "Okay, now my turn! Think about some number from 1 to 100\n";
+		isGuessed = false;
+		int computerAmountOfTries = 0;
+		int min = 1;
+		int max = 100;
+		while (!isGuessed)
+		{
+			++computerAmountOfTries;
+			int guess = (min + max) / 2;
+			std::cout << "My try: " << guess << "\n";
+			std::cout << "Is it correct? (y/n)\n";
+			std::string answer;
+			std::cin >> answer;
+			if (answer == "y")
+			{
+				std::cout << "I guessed it in " << computerAmountOfTries << " tries!\n";
+				isGuessed = true;
+			}
+			else
+			{
+				std::cout << "Is it too low? (y/n)\n";
+				std::cin >> answer;
+				if (answer == "y")
+				{
+					min = guess + 1;
+				}
+				else
+				{
+					max = guess - 1;
+				}
+
+				if (min >= max)
+				{
+					std::cout << "You are cheating!\n";
+					break;
+				}
+			}
+		}
+
+		if (computerAmountOfTries < playerAmountOfTries)
+		{
+			std::cout << "I won!\n";
+		}
+		else if (computerAmountOfTries > playerAmountOfTries)
+		{
+			std::cout << "You won!\n";
+		}
+		else
+		{
+			std::cout << "It's a draw!\n";
 		}
 
 		std::cout << "Do you want to play again? (y/n)\n";
